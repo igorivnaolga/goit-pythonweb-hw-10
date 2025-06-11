b01976680b7870951c01198f4f0b15ffed496596
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2025-06-11 11:21:51 +0300

    add user to contacts

--- src/repository/contacts.py
+++ src/repository/contacts.py
@@ -22,26 +22,28 @@ class ContactRepository:
         email: str | None,
         user: User,
     ) -> List[Contact]:
-        stmt = select(Contact)
+        stmt = select(Contact).filter(Contact.user_id == user.id)
         if name:
-            stmt = stmt.filter_by(first_name=name, user=user)
+            stmt = stmt.filter(first_name=name)
         if surname:
-            stmt = stmt.filter_by(last_name=surname, user=user)
+            stmt = stmt.filter(last_name=surname)
         if email:
-            stmt = stmt.filter_by(email=email, user=user)
+            stmt = stmt.filter(email=email)
         stmt.offset(skip).limit(limit)
         result = await self.db.execute(stmt)
         return result.scalars().all()
 
-    async def get_contact(self: Self, contact_id: int, user=User) -> Contact | None:
-        stmt = select(Contact).filter_by(id=contact_id, user=user)
+    async def get_contact(self: Self, contact_id: int, user: User) -> Contact | None:
+        stmt = select(Contact).filter(
+            Contact.user_id == user.id, Contact.id == contact_id
+        )
         result = await self.db.execute(stmt)
         return result.scalar_one_or_none()
 
     async def create_contact(
-        self: Self, body: ContactBase, user=User
+        self: Self, body: ContactBase, user: User
     ) -> Contact | None:
-        contact = Contact(**body.model_dump(exclude_unset=True), user=User)
+        contact = Contact(**body.model_dump(exclude_unset=True), user=user)
         self.db.add(contact)
         await self.db.commit()
         await self.db.refresh(contact)
@@ -56,19 +58,8 @@ class ContactRepository:
         await self.db.commit()
         return True
 
-    # async def update_contact(
-    #     self: Self, contact_id: int, body: ContactBase
-    # ) -> Contact | None:
-    #     contact = await self.get_contact(contact_id)
-    #     if contact:
-    #         for key, value in body.model_dump().items():
-    #             setattr(contact, key, value)
-    #         await self.db.commit()
-    #         await self.db.refresh(contact)
-    #     return contact
-
     async def update_contact(
-        self, contact_id: int, user: User, body: ContactUpdate
+        self: Self, contact_id: int, body: ContactUpdate, user: User
     ) -> Contact | None:
         contact = await self.get_contact(contact_id, user)
         if contact:
@@ -78,37 +69,38 @@ class ContactRepository:
             await self.db.refresh(contact)
         return contact
 
-    async def get_birthdays(self: Self, skip: int, limit: int) -> List[Contact]:
+    async def get_birthdays(
+        self: Self, skip: int, limit: int, user: User
+    ) -> List[Contact]:
         today = datetime.now().date()
-        today_month = today.month
-        today_day = today.day
-
         end_date = today + timedelta(days=7)
-        end_month = end_date.month
-        end_day = end_date.day
 
         stmt = (
             select(Contact)
-            .filter(
+            .where(
+                Contact.user_id == user.id,
                 or_(
+                    # Дні народження в тому ж місяці
                     and_(
-                        extract("month", Contact.birthday) == today_month,
-                        extract("day", Contact.birthday) >= today_day,
+                        extract("month", Contact.birthday) == today.month,
+                        extract("day", Contact.birthday) >= today.day,
                     ),
+                    # Дні народження в наступному місяці (в межах 7 днів)
                     and_(
-                        extract("month", Contact.birthday) == end_month,
-                        extract("day", Contact.birthday) <= end_day,
+                        extract("month", Contact.birthday) == end_date.month,
+                        extract("day", Contact.birthday) <= end_date.day,
                     ),
-                    # якщо перехід через грудень → січень
+                    # Якщо сьогодні грудень, а кінець діапазону — січень
                     and_(
+                        today.month == 12,
                         extract("month", Contact.birthday) == 1,
-                        today_month == 12,
-                        extract("day", Contact.birthday) <= end_day,
+                        extract("day", Contact.birthday) <= end_date.day,
                     ),
-                )
+                ),
             )
             .offset(skip)
             .limit(limit)
         )
+
         result = await self.db.execute(stmt)
         return result.scalars().all()
--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -6,6 +6,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.database.db import get_db
 from src.schemas.contacts import ContactBase, ContactResponse, ContactUpdate
 from src.services.contacts import ContactService
+from src.services.auth import auth_service
+from src.database.models import User
 
 router = APIRouter(prefix="/contacts", tags=["contacts"])
 
@@ -18,9 +20,12 @@ async def get_contacts(
     skip: int = 0,
     limit: int = Query(10, le=1000),
     db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
 ):
     contact_service = ContactService(db)
-    contacts = await contact_service.get_contacts(skip, limit, name, surname, email)
+    contacts = await contact_service.get_contacts(
+        skip, limit, name, surname, email, user
+    )
     return contacts
 
 
@@ -31,16 +36,21 @@ async def get_upcomming_birthdays(
     skip: int = Query(0, ge=0),
     limit: int = Query(10, le=1000),
     db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
 ):
     contact_service = ContactService(db)
-    contacts = await contact_service.birthdays(skip, limit)
+    contacts = await contact_service.birthdays(skip, limit, user)
     return contacts
 
 
 @router.get("/{contact_id}", response_model=ContactResponse)
-async def read_contact(contact_id: int, db: AsyncSession = Depends(get_db)):
+async def read_contact(
+    contact_id: int = Path(ge=1),
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
+):
     contact_service = ContactService(db)
-    contact = await contact_service.get_contact(contact_id)
+    contact = await contact_service.get_contact(contact_id, user)
     if contact is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found"
@@ -49,17 +59,27 @@ async def read_contact(contact_id: int, db: AsyncSession = Depends(get_db)):
 
 
 @router.post("/", response_model=ContactResponse, status_code=status.HTTP_201_CREATED)
-async def create_contact(body: ContactBase, db: AsyncSession = Depends(get_db)):
+async def create_contact(
+    body: ContactBase,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
+):
     contact_service = ContactService(db)
-    return await contact_service.create_contact(body)
+    contact = await contact_service.create_contact(body, user)
+    if contact is None:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Email in use")
+    return contact
 
 
 @router.patch("/{contact_id}", response_model=ContactResponse)
 async def update_contact(
-    contact_id: int, body: ContactUpdate, db: AsyncSession = Depends(get_db)
+    contact_id: int,
+    body: ContactUpdate,
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
 ):
     contact_service = ContactService(db)
-    contact = await contact_service.update_contact(contact_id, body)
+    contact = await contact_service.update_contact(contact_id, body, user)
     if contact is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found"
@@ -69,10 +89,12 @@ async def update_contact(
 
 @router.delete("/{contact_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_contact(
-    contact_id: int = Path(ge=1), db: AsyncSession = Depends(get_db)
+    contact_id: int = Path(ge=1),
+    db: AsyncSession = Depends(get_db),
+    user: User = Depends(auth_service.get_current_user),
 ):
     contact_service = ContactService(db)
-    contact = await contact_service.delete_contact(contact_id)
+    contact = await contact_service.delete_contact(contact_id, user)
     if contact is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Contact not found"
